b617add66a29edb1f2174690813c51302a161094
Liam Andrew Ernst <lae5777@multi-vision.cis.rit.edu>
2025-06-05 13:25:05 -0400

    gazebo launch loads circuit world w/ hospitalbot

--- hospital_robot_spawner/launch/gazebo_world.launch.py
+++ hospital_robot_spawner/launch/gazebo_world.launch.py
@@ -14,8 +14,8 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
-    world_file_name = 'hospital.world'
-    pkg_dir = get_package_share_directory('hospital_robot_spawner')
+    world_file_name = 'circuit.world'
+    pkg_dir = get_package_share_directory('test_new_pkg_name')
 
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
     #os.environ["GAZEBO_RESOURCE_PATH"] = os.path.join(pkg_dir, 'worlds')
@@ -32,7 +32,7 @@ def generate_launch_description():
     #spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
     #                    arguments=['-entity', 'demo', 'x', 'y', 'z'],
     #                    output='screen')
-    spawn_entity = Node(package='hospital_robot_spawner', executable='spawn_demo',
+    spawn_entity = Node(package='test_new_pkg_name', executable='spawn_demo',
                         arguments=['HospitalBot', 'demo', '1', '16.0', '0.0'],
                         output='screen')
 
