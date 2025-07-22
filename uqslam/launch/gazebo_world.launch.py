fb4b3efcda11ace760a04c3faede9f16668f59f4
Rohaan Nadeem <rn7823@g.rit.edu>
2025-07-22 15:56:15 -0400

    changes from Liam (I think)

--- uqslam/launch/gazebo_world.launch.py
+++ uqslam/launch/gazebo_world.launch.py
@@ -7,11 +7,10 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
-
 
 def generate_launch_description():
     ld = []
@@ -20,32 +19,23 @@ def generate_launch_description():
     pkg_dir = get_package_share_directory('uqslam')
 
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
-    # os.environ["GAZEBO_RESOURCE_PATH"] = os.path.join(pkg_dir, 'worlds')
+    #os.environ["GAZEBO_RESOURCE_PATH"] = os.path.join(pkg_dir, 'worlds')
 
     world = os.path.join(pkg_dir, 'worlds', world_file_name)
     launch_file_dir = os.path.join(pkg_dir, 'launch')
 
-    # gazebo = ExecuteProcess(
-    #         cmd=['gzserver', '--verbose', '--headless-rendering', world, '-s', 'libgazebo_ros_init.so',
-    #         '-s', 'libgazebo_ros_factory.so'],
-    #         output='screen')
     ld.append(ExecuteProcess(
         cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so',
              '-s', 'libgazebo_ros_factory.so'],
         output='screen'))
 
     # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
-    # spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
+    #spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
     #                    arguments=['-entity', 'demo', 'x', 'y', 'z'],
     #                    output='screen')
-    for r in range(4):
-        for c in range(4):
-            ld.append(Node(package='uqslam', executable='spawn_demo',
-                           arguments=['HospitalBot', f'simulation_{r*4+c}', '1',
-                                      str(100.0 + c*2.0), str(-100.0 + r*2.0)], output='screen'))
-
-    # spawn_entity = Node(package='uqslam', executable='spawn_demo',
-    #                     arguments=['HospitalBot', 'demo', '1', '16.0', '0.0'],
-    #                     output='screen')
-
+            
+    spawn_entity = Node(package='uqslam', executable='spawn_demo',
+                        arguments=['HospitalBot', 'demo', '1', '16.0', '0.0'],
+                        output='screen')
+    ld.append(spawn_entity)
     return LaunchDescription(ld)
